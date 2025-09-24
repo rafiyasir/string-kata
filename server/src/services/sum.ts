eb398d680d384eb0de643c4d3da0828ae677d637
rafiuzzaman202 <rafiuzzaman202@gmail.com>
2025-09-24 18:24:53 +0530

    tested multiple condition

--- server/src/services/sum.ts
+++ server/src/services/sum.ts
@@ -1,5 +1,11 @@
-export function sum(str: string){
-if(!str){
-    return 0
-}
+export function sum(str: string) {
+    if (!str) {
+        return 0
+    }
+    const strArr = str.split(',');
+    let sum = 0;
+    for (let num of strArr) {
+        sum += Number(num);
+    }
+    return sum;
 }
--- server/tests/unit/sum.test.ts
+++ server/tests/unit/sum.test.ts
@@ -4,5 +4,13 @@ describe("String Kata",()=>{
     it("should return 0", () => {
         const res = sum("");
         expect(res).toEqual(0);
+    });
+    it("should return 1 for '1' ",()=>{
+        const res = sum("1");
+        expect(res).toEqual(1)
+    })
+    it("should return 6 for '1,5'",()=>{
+        const res = sum("1,5");
+        expect(res).toEqual(6)
     })
 })
