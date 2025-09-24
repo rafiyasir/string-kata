6c29c083aeed03ec38d88d776518641978314fdc
rafiuzzaman202 <rafiuzzaman202@gmail.com>
2025-09-24 17:36:14 +0530

    first fail test

--- server/tests/unit/sum.test.ts
+++ server/tests/unit/sum.test.ts
@@ -1,5 +1,8 @@
+import { sum } from "../../src/services/sum"
+
 describe("String Kata",()=>{
     it("should return 0", () => {
-        
+        const res = sum("");
+        expect(res).toEqual(0);
     })
 })
