7dd22790dba21eb4792fa089f365289ea8ae0fc8
rafiuzzaman202 <rafiuzzaman202@gmail.com>
2025-09-24 22:35:29 +0530

    updated integration test

--- server/src/controller/sum.ts
+++ server/src/controller/sum.ts
@@ -1,7 +1,14 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { sum } from "../services/sum";
 
 export const getSum = (req: Request, res: Response) => {
-    const total = sum(req.query.str as string);
-    return res.status(200).json({total})
+    try {
+        if (!req.query.str) {
+            return res.status(400).json({ message: "Invalid request" })
+        }
+        const total = sum(req.query.str as string);
+        return res.status(200).json({ total })
+    } catch (error) {
+        res.status(400).json(error)
+    }
 }
--- server/tests/integration/app.test.ts
+++ server/tests/integration/app.test.ts
@@ -15,7 +15,14 @@ afterAll(async () => {
 
 describe("String Kata API", () => {
     it("should return 200 for /api/sum", async () => {
-        const res = await request(server).get('/api/sum');
+        const res = await request(server).get('/api/sum').query({str:"1,2"});
         expect(res.status).toBe(200);
-    })
+    });
+
+    it("should return 400 for not having query", async () => {
+        const res = await request(server).get('/api/sum');
+        expect(res.status).toBe(400);
+    });
+
+
 })
